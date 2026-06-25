fe5f7e403e88e9330864c9807e0c8899bfa6f86d
mohnishbendwal07 <mohnishbendwal07@gmail.com>
2026-06-25 18:24:01 +0530

    Add home route

--- Backend/server.js
+++ Backend/server.js
@@ -12,6 +12,10 @@ const authRoutes = require("./Routes/auth");
 app.use("/tasks", taskRoutes);
 app.use("/auth", authRoutes);
 
+app.get("/", (req, res) => {
+  res.send("Task Manager Backend is Running 🚀");
+});
+
 app.listen(5000, () => {
     console.log("Server running on port 5000");
 });
